3c1b5dc42074783c6d3b26e0b3e880111a1c4fa6
Ulrich Dangel <uli@spamt.net>
2011-03-10 23:52:05 +0100

    Support non javascript browsers as well

--- static/script.js
+++ static/script.js
@@ -27,7 +27,7 @@ function prettify_feedback(feedback) {
 
   function prepareEntry(kind) {
     return function() {
-      $('input[name="kind"]').val(kind);
+      $('select[name="kind"]').val(kind);
       $('.kind-selector', feedback).removeClass('active-kind');
       $(this).parent().addClass('active-kind');
       $($("li", $('.feedback'))[1]).show()
@@ -44,8 +44,6 @@ function prettify_feedback(feedback) {
     nextStep.slideUp('fast');
   });
 
-  $('a.happy', feedback).click(prepareEntry('h'));
-  $('a.unhappy', feedback).click(prepareEntry('u'));
   function validateVersion(elem) {
     if (elem.value.length > 0 && ! elem.value.match(/^(\d{4}\.\d{2})|(daily)$/)) {
       showFormError('Wrong version. Use YEAR.MONTH (2010.12) or daily');
@@ -72,6 +70,12 @@ function prettify_feedback(feedback) {
       return false;
     }
   });
+  $('select[name="kind"]').hide();
+  $('.selectors').append('<span class="kind-selector"><a href=# class=happy>happy ☺</a></span> / <span class="kind-selector"><a href=# class=unhappy>unhappy ☹</a></span>');
+  $('a.happy', feedback).click(prepareEntry('h'));
+  $('a.unhappy', feedback).click(prepareEntry('u'));
+
+
 }
 
 $(function() {
